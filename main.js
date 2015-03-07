f2a6adde0ab97125363366e2e7507366d511bc53
Objelisks <supertyrian@gmail.com>
2015-03-07 01:33:42 -0800

    update

--- main.js
+++ main.js
@@ -2,35 +2,75 @@ var display = new ROT.Display({width:80, height:40, bg:"#304030", fontStyle:"bol
 document.body.appendChild(display.getContainer());
 
 
-var engine = new ROT.Engine();
+var input = document.createElement("input");
+input.focus();
+var keys = {};
 
-var delay = function(self, func, params, milli) {
-	setTimeout(function() { func.apply(self, params); }, milli);
+
+var scheduler = new ROT.Scheduler.Simple();
+var engine = new ROT.Engine(scheduler);
+
+var map = {
+	tiles: {},
+	act: function() {
+		console.log("frame");
+		display.clear();
+		Object.keys(this.tiles).forEach(function(key) {
+			var parts = key.split(',');
+			var x = parseInt(parts[0]);
+			var y = parseInt(parts[1]);
+			display.draw(x, y, "#");
+		});
+	}
 };
 
-var draw = display.draw.bind(display);
+map.tiles['0,0'] = true;
+map.tiles['0,1'] = true;
+map.tiles['1,0'] = true;
+map.tiles['2,0'] = true;
 
-delay(display, draw, [1, 1, "#"], 50);
-delay(display, draw, [2, 1, "#"], 150);
-delay(display, draw, [4, 4, "#"], 250);
-delay(display, draw, [4, 5, "#"], 350);
-delay(display, draw, [5, 4, "#"], 450);
-delay(display, draw, [5, 5, "#"], 550);
-delay(display, draw, [10, 2, "#"], 650);
-delay(display, draw, [18, 5, "#"], 750);
-delay(display, draw, [60, 10, "#"], 850);
+var moveInput = function(obj) {
+	window.addEventListener("keydown", obj);
+};
 
+var player = {
+	x: 5,
+	y: 5,
+	act: function() {
+		display.draw(this.x, this.y, "@");
+		engine.lock();
+		moveInput(this);
+	},
+	handleEvent: function(e) {
+		var code = e.keyCode;
+		switch(code) {
+			case ROT.VK_RIGHT:
+				this.x += 1;
+				break;
+			case ROT.VK_LEFT:
+				this.x -= 1;
+				break;
+			case ROT.VK_UP:
+				this.y -= 1;
+				break;
+			case ROT.VK_DOWN:
+				this.y += 1;
+				break;
+			default:
+				return;
+		}
+		this.clearInput();
+	},
+	clearInput: function() {
+		window.removeEventListener("keydown", this);
+		engine.unlock();
+	}
+};
 
-delay(display, draw, [1, 1, " "], 1150);
-delay(display, draw, [2, 1, " "], 1250);
-delay(display, draw, [4, 4, " "], 1350);
-delay(display, draw, [4, 5, " "], 1450);
-delay(display, draw, [5, 4, " "], 1550);
-delay(display, draw, [5, 5, " "], 1650);
-delay(display, draw, [10, 2, " "], 1750);
-delay(display, draw, [18, 5, " "], 1850);
-delay(display, draw, [60, 10, " "], 1950);
+scheduler.add(player, true);
+scheduler.add(map, true);
 
+engine.start();
 
 // game loop
 // entities
@@ -38,3 +78,10 @@ delay(display, draw, [60, 10, " "], 1950);
 // items
 // key bindings
 // movement
+
+// text boxes
+// ui elements
+// colored text
+// multitile characters
+// particle effects
+// swarming
